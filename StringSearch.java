03b319762dd05076dee23026ea94b46f886c0390
Victor Sannicolo <victor.sannicolo.5554@student.lu.se>
2024-12-03 11:49:18 +0100

    Implemented StringSearch
    Co-authored-by: emelietingberg <emelietingberg@users.noreply.github.com>

--- StringSearch.java
+++ StringSearch.java
@@ -1,8 +1,62 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
 
 public class StringSearch {
 
+    private static Scanner scan = new Scanner(System.in);
+    
+    /**
+     * Main method that reads user input and calls search method.
+     */
     public static void main(String[] args) {
-        System.out.println("test");
+        String input = scan.nextLine();
+        String[] inputArray = input.split(" "); 
+
+        if (inputArray.length != 3) {
+            System.out.println("Invalid input. Enter: search <pattern> <filename>");
+        }
+        else if (inputArray[0].equalsIgnoreCase("search")) {
+            search(inputArray[1], inputArray[2]);
+        } else {
+            System.out.println("Invalid command. Only \"search\" is supported");
+        }
     }
+
+    /**
+     * Searches for a word in a file and prints line numbers where the word is found.
+     * @param word
+     * @param file
+     */
+    public static void search(String word, String file){
+        try {
+        ArrayList<String> lines = new ArrayList<String>();
+        Scanner inputFile = new Scanner(new File(file));
+        int countLine = 1; 
+
+        while (inputFile.hasNextLine()){
+            String line = inputFile.nextLine();
+            String[] words = line.split(" ");
+            for (String s : words){
+                if (s.equalsIgnoreCase(word)){
+                    lines.add(Integer.toString(countLine)); 
+                }
+            }
+            countLine++; 
+        }
+
+        if (lines.isEmpty()){
+            System.out.println("Word not found in file");
+        } else {
+            System.out.println("Word found on lines: " + lines);
+        }
+        inputFile.close(); 
+        
+        } catch (Exception e) {
+            System.out.println("File not found"); 
+        }
+    }
+
+
 
 }
