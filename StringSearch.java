7f06a8d8dbbb9edb5c2e7c0171e46484b4c15023
Victor Sannicolo <victor.sannicolo.5554@student.lu.se>
2024-12-03 11:05:04 +0100

    Test branch

--- StringSearch.java
+++ StringSearch.java
@@ -2,7 +2,7 @@
 public class StringSearch {
 
     public static void main(String[] args) {
-        System.out.println("Test"); 
+        System.out.println("Hejsan från branch \"Test\""); 
     }
 
 }
